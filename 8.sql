ee427d948eae7b902a273627d6803d8ae7f9ca4a
Jon Huffaker <jonathanhuffaker@gmail.com>
2016-01-17 13:14:52 -0600

    added some comments to #8 so I would know the difference between my two answers

--- 8.sql
+++ 8.sql
@@ -5,6 +5,8 @@
 SELECT Invoice.Total, Customer.FirstName, Customer.LastName, Customer.Country, Employee.FirstName, Employee.LastName FROM Invoice JOIN Customer ON (Invoice.CustomerId == Customer.CustomerId) JOIN Employee ON (Customer.SupportRepId == Employee.EmployeeId)
 
 
+-- Column names now reflecting the first name lastname by rep and customer
+
 select Invoice.Total, Customer.FirstName as 'Customer First Name', Customer.LastName as 'Customer Last Name', Customer.Country as 'Customer Country', Employee.FirstName as 'Employee FirstName', Employee.LastName as 'Employee Last Name'
 from Invoice JOIN Customer ON (Invoice.CustomerId == Customer.CustomerId) JOIN Employee ON (Customer.SupportRepId == Employee.EmployeeId)
 
