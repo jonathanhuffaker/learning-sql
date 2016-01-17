84752b5a90d03134ad634f4a9e48cf0f19dba384
Jon Huffaker <jonathanhuffaker@gmail.com>
2016-01-17 13:13:07 -0600

    Number 8 complete

---
+++ 8.sql
@@ -0,0 +1,10 @@
+-- 8. Provide a query that shows the Invoice Total, Customer name, Country and Sale Agent name for all invoices and customers. 
+
+
+-- Below works but does not distinguish the difference between cusomter/employee first and last names
+SELECT Invoice.Total, Customer.FirstName, Customer.LastName, Customer.Country, Employee.FirstName, Employee.LastName FROM Invoice JOIN Customer ON (Invoice.CustomerId == Customer.CustomerId) JOIN Employee ON (Customer.SupportRepId == Employee.EmployeeId)
+
+
+select Invoice.Total, Customer.FirstName as 'Customer First Name', Customer.LastName as 'Customer Last Name', Customer.Country as 'Customer Country', Employee.FirstName as 'Employee FirstName', Employee.LastName as 'Employee Last Name'
+from Invoice JOIN Customer ON (Invoice.CustomerId == Customer.CustomerId) JOIN Employee ON (Customer.SupportRepId == Employee.EmployeeId)
+
