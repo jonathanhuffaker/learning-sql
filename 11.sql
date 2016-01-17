5c6ccca1a4afa39d8426738fe39a5dbd6aabc24b
Jon Huffaker <jonathanhuffaker@gmail.com>
2016-01-17 17:08:29 -0600

    re wrote 11.sql

--- 11.sql
+++ 11.sql
@@ -1,4 +1,6 @@
 -- 11. Looking at the InvoiceLine table, provide a query that COUNTs the number of line items for each Invoice. HINT: [GROUP BY](http://www.sqlite.org/lang_select.html#resultset)
 
 
-Select InvoiceId, COUNT(InvoiceId) from InvoiceLine GROUP BY InvoiceId
+-- Select InvoiceId, COUNT(InvoiceId) from InvoiceLine GROUP BY InvoiceId
+
+Select InvoiceId, COUNT(InvoiceId) AS "Number of Invoices" FROM InvoiceLine GROUP BY InvoiceId
