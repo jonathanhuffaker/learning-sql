ec04dbe9755dbf3d37b8a42640642d78f0cefbdc
Jon Huffaker <jonathanhuffaker@gmail.com>
2016-01-17 21:08:41 -0600

    Updated column name to reflect # of invoice line items

--- 17.sql
+++ 17.sql
@@ -1,3 +1,3 @@
 --17. Provide a query that shows all Invoices but includes the # of invoice line items.
 
-SELECT Invoice.InvoiceId, COUNT(InvoiceLine.InvoiceLineId) FROM Invoice JOIN InvoiceLine On (InvoiceLine.InvoiceId == Invoice.InvoiceId) GROUP BY Invoice.InvoiceId
+SELECT Invoice.InvoiceId, COUNT(InvoiceLine.InvoiceLineId) AS "Number of Inoice Line Items" FROM Invoice JOIN InvoiceLine On (InvoiceLine.InvoiceId == Invoice.InvoiceId) GROUP BY Invoice.InvoiceId
