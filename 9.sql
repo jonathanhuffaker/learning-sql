5d0840456cd0df0a2240faca536fd8c2990758d2
Jon Huffaker <jonathanhuffaker@gmail.com>
2016-01-25 19:28:06 -0600

    Forgot to include total sales in my query.  Now complete

--- 9.sql
+++ 9.sql
@@ -4,5 +4,11 @@
 -- SELECT COUNT (*) FROM Invoice WHERE InvoiceDate >= 2009-1-1 00:00:00 AND InvoiceDate<=2012-1-1 00:00:00
 
 
+
 SELECT substr(InvoiceDate,1,4) as "Year", COUNT(InvoiceId) as "Number of Invoices" FROM Invoice WHERE InvoiceDate LIKE '%2009%' UNION
 SELECT substr(InvoiceDate,1,4), COUNT(InvoiceId) FROM Invoice WHERE InvoiceDate LIKE '%2011%'
+
+-- ================below includestotal sales
+
+SELECT substr(InvoiceDate,1,4) as "Year", COUNT(InvoiceId) as "Number of Invoices", COUNT(*), ROUND(SUM(Total),2) FROM Invoice WHERE InvoiceDate LIKE '%2009%' UNION
+SELECT substr(InvoiceDate,1,4), COUNT(InvoiceId), COUNT(*), ROUND(SUM(Total),2) FROM Invoice WHERE InvoiceDate LIKE '%2011%'
